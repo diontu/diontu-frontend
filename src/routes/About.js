db37bbe28dd08ef66090f7a272f8a830908f897f
Dion <diontugit@gmail.com>
2020-12-27 22:58:34 -0500

    Updated About and Home page, Updated TeachStack component styles

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
-
-const techStack = ["React", "NodeJS", "Java", "C", "MongoDB", "PostgreSQL", "Git", "Docker", "AWS", "Heroku", "VSCode", "Confluence", "Figma"]
+import TechStack from "./../components/TechStack"
 
 class About extends Component {
   render() {
@@ -9,25 +8,21 @@ class About extends Component {
         <h1>About</h1>
         <div>
           <p>Hi, I'm Dion Tu.</p>
+          {/* explain what backend means to me */}
           <p>
-            I'm an aspiring Web Developer and a Backend Solutions enthusiast. I've
-            worked as a developer at TD Bank, maintainting and adding features to an internal web
-            application using both frontend and backend technologies. I was also a Research Intern
-            for an audio research lab at York University, designing and maintaining the public
-            website, as well as contributing to a large-scale dataset. I'm currently in my final
-            year at the Lassonde School of Engineering at York University with expected graduation
-            date of May 2021.
+            I'm an aspiring Web Developer and a Backend Solutions enthusiast. I've worked as a
+            developer at TD Bank, maintaining and adding features to an internal web application
+            using both frontend and backend technologies. I was also a Research Intern for an audio
+            research lab at York University, designing and maintaining the public website, as well
+            as contributing to a large-scale dataset. I'm currently in my final year at the Lassonde
+            School of Engineering at York University with expected graduation date of May 2021.
           </p>
           <p>
-            I like using different technologies to help me efficiently build cool things and document them, but I
-            generally use the following technologies:
+            I like using different technologies to help me efficiently build cool things and
+            document them, but I generally use the following technologies:
           </p>
-          <ul>
-            {techStack.map((tech) => (
-              <li key={tech}>{tech}</li>
-            ))}
-          </ul>
         </div>
+        <TechStack />
       </div>
     )
   }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,8 +7,8 @@ class Home extends Component {
       <div>
         <h1>Hi! I'm Dion Tu.</h1>
         <p>
-          This is my personal website where I showcase my projects and write about 
-          technical topics in my blog that I find are interesting.
+          This is my personal website where I showcase my projects and write about technical topics
+          in my blog that I find are interesting.
         </p>
         <TechStack />
       </div>
