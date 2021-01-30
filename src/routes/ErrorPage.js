576794f934e3a8da57c4f746bfd8ba985a3f8b14
Dion <diontugit@gmail.com>
2021-01-30 00:35:32 -0500

    prettier config ran

--- src/routes/ErrorPage.js
+++ src/routes/ErrorPage.js
@@ -2,13 +2,9 @@ import React, { Component } from "react"
 import { withRouter } from "react-router-dom"
 
 class ErrorPage extends Component {
-    render() {
-        return(
-            <div style={{ color: "red" }}>
-                404 Error... Could not find what you're looking for.
-            </div>
-        )
-    }
+  render() {
+    return <div style={{ color: "red" }}>404 Error... Could not find what you're looking for.</div>
+  }
 }
 
 export default ErrorPage
